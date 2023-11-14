f5ad0613b0489302373efa29b92e8f9ec50495d3
Radekminecraft <radomirvondra@gmail.com>
2023-11-14 14:25:42 +0100

    pistol pew pew

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int bulletDamage;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //TODO: Fix collisions (maybe layers?)
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health) health.Damage(bulletDamage);
+    }
+}
---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [SerializeField] private GameObject pistol;
+    [SerializeField] private GameObject shootPoint;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float speed;
+    [SerializeField] private float despawnTime;
+    private Transform shootPointTr;
+    private void Start()
+    {
+        shootPointTr = shootPoint.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject spawnedBullet = Instantiate(bullet, shootPointTr.position, pistol.GetComponent<Transform>().rotation);
+            spawnedBullet.GetComponent<Rigidbody2D>().AddForce(shootPointTr.right * speed, ForceMode2D.Impulse);
+            Destroy(spawnedBullet, despawnTime);
+        }
+    }
+}
